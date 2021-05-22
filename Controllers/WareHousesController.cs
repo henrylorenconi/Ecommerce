3d72c3a204ded64c665a6f70e0f79ceddd4c3d4b
henrylorenconi <henrylorenconi05@gmail.com>
2021-05-21 23:57:14 -0300

    WareHouse Controller Was Created

---
+++ Controllers/WareHousesController.cs
@@ -0,0 +1,146 @@
+﻿using ECommerce.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace ECommerce.Controllers
+
+{
+    [Authorize(Roles = "User , Admin")]
+
+    public class WareHousesController : Controller
+    {
+        private EcommerceContext db = new EcommerceContext();
+
+        // GET: WareHouses
+        public ActionResult Index()
+        {
+            var user = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+
+            if (user == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var wareHouses = db.WareHouses.Where(w => w.CompanyId == user.CompanyId).Include(w => w.Cities).Include(w => w.Departaments);
+            return View(wareHouses.ToList());
+        }
+
+        // GET: WareHouses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WareHouse wareHouse = db.WareHouses.Find(id);
+            if (wareHouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wareHouse);
+        }
+
+        // GET: WareHouses/Create
+        public ActionResult Create()
+        {
+            ViewBag.CityId = new SelectList(db.Cities, "CityId", "Name");
+            ViewBag.CompanyId = new SelectList(db.Companies, "CompanyId", "Name");
+            ViewBag.DepartamentsId = new SelectList(db.Departaments, "DepartamentsId", "Name");
+            return View();
+        }
+
+        // POST: WareHouses/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WareHouseId,CompanyId,Name,Phone,Address,DepartamentsId,CityId")] WareHouse wareHouse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WareHouses.Add(wareHouse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CityId = new SelectList(db.Cities, "CityId", "Name", wareHouse.CityId);
+            ViewBag.CompanyId = new SelectList(db.Companies, "CompanyId", "Name", wareHouse.CompanyId);
+            ViewBag.DepartamentsId = new SelectList(db.Departaments, "DepartamentsId", "Name", wareHouse.DepartamentsId);
+            return View(wareHouse);
+        }
+
+        // GET: WareHouses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WareHouse wareHouse = db.WareHouses.Find(id);
+            if (wareHouse == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityId = new SelectList(db.Cities, "CityId", "Name", wareHouse.CityId);
+            ViewBag.CompanyId = new SelectList(db.Companies, "CompanyId", "Name", wareHouse.CompanyId);
+            ViewBag.DepartamentsId = new SelectList(db.Departaments, "DepartamentsId", "Name", wareHouse.DepartamentsId);
+            return View(wareHouse);
+        }
+
+        // POST: WareHouses/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WareHouseId,CompanyId,Name,Phone,Address,DepartamentsId,CityId")] WareHouse wareHouse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(wareHouse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CityId = new SelectList(db.Cities, "CityId", "Name", wareHouse.CityId);
+            ViewBag.CompanyId = new SelectList(db.Companies, "CompanyId", "Name", wareHouse.CompanyId);
+            ViewBag.DepartamentsId = new SelectList(db.Departaments, "DepartamentsId", "Name", wareHouse.DepartamentsId);
+            return View(wareHouse);
+        }
+
+        // GET: WareHouses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WareHouse wareHouse = db.WareHouses.Find(id);
+            if (wareHouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wareHouse);
+        }
+
+        // POST: WareHouses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WareHouse wareHouse = db.WareHouses.Find(id);
+            db.WareHouses.Remove(wareHouse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

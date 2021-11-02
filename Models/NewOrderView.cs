5e9d4a0b15253dc3ccc9c7f5ab0cb900df16107b
henrylorenconi <henrylorenconi05@gmail.com>
2021-11-02 12:35:36 -0300

    teste

--- Models/NewOrderView.cs
+++ Models/NewOrderView.cs
@@ -26,7 +26,7 @@ namespace ECommerce.Models
         [Display(Name = "Detalhes")]
         public List<OrderDetailTmp> Details { get; set; }
 
-        //[DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public double TotalQuantity { get { return Details == null ? 0 : Details.Sum(d => d.Quantity); } }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
